4f7d22c3b4619c50198e8435f094fe96532fd758
Anirudha Gandhare <anirudha@chisellabs.com>
2023-02-19 18:04:49 +0530

    feat: loader fix

--- client/src/shared/components/Layout.jsx
+++ client/src/shared/components/Layout.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
-import Loader from './Loader';
 
 function Layout() {
     const { isCoreInitializing } = useSelector(state => state.core);
     if (isCoreInitializing) {
         return (
             <div className='h-screen flex flex-col items-center justify-center gap-4'>
-                <Loader size={40} />
+                <div className="loader ease-linear rounded-full border border-t border-gray-200 h-[40px] w-[40px]"></div>
                 <p className="font-semibold heading-text">
                     Loading...
                 </p>
--- client/src/shared/components/Loader.jsx
+++ client/src/shared/components/Loader.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Loader({ size = 18 }) {
+function Loader() {
     return (
-        <div className={`loader ease-linear rounded-full border border-t border-gray-200 h-[${size}px] w-[${size}px]`}></div>
+        <div className="loader ease-linear rounded-full border border-t border-gray-200 h-[18px] w-[18px]"></div>
     )
 }
 
-export default Loader;
+export default Loader
