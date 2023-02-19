f07b3d127c60a82f806f9d6e23c5a40f2311eca5
Anirudha Gandhare <anirudha@chisellabs.com>
2023-02-19 17:57:07 +0530

    feat: loader fix

--- client/src/shared/components/Loader.jsx
+++ client/src/shared/components/Loader.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Loader({ size, stroke = 1 }) {
+function Loader({ size = 18 }) {
     return (
         <div className={`loader ease-linear rounded-full border border-t border-gray-200 h-[${size}px] w-[${size}px]`}></div>
     )
 }
 
-export default Loader
+export default Loader;
