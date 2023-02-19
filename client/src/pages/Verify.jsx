5a530b9b157bf19b8c2f3317b10c834672672167
Anirudha Gandhare <anirudha@chisellabs.com>
2023-02-19 16:12:43 +0530

    feat: icon change

--- client/src/pages/Verify.jsx
+++ client/src/pages/Verify.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { SiMinutemailer } from'react-icons/si';
+import { MdOutlineAdminPanelSettings } from'react-icons/md';
 import { useSelector } from 'react-redux';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { currentUserSelector } from '../selectors/current';
@@ -17,7 +17,7 @@ function Verify() {
   }
   return (
     <div className='h-screen w-full flex items-center justify-center flex-col'>
-        <SiMinutemailer size={100} className="text-slate-700" />
+        <MdOutlineAdminPanelSettings size={100} className="text-slate-700" />
         <div className="heading-text text-6xl text-slate-700 font-bold mt-4 mb-8">In Review</div>
         <div className="max-w-[900px] text-md text-slate-500">Please check after sometime. Account will be reviewed by Admin</div>
     </div>
